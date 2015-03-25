575d9d66063b35099dd561e80844e0a2c1531fe5
amiguel <alejandro.miguel.herrero@gmail.com>
2015-03-25 17:16:57 +0100

    Add REST services
    Adds services to find, save and delete books in the library.

---
+++ lab-webapp/src/main/java/com/ethertion/lab/webapp/pojo/Book.java
@@ -0,0 +1,35 @@
+package com.ethertion.lab.webapp.pojo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author amiguel
+ */
+public class Book implements Serializable{
+        
+        private String title;
+        private String author;
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public String getAuthor() {
+                return author;
+        }
+
+        public void setAuthor(String author) {
+                this.author = author;
+        }
+
+        @Override
+        public String toString() {
+                return "Book{" + "title=" + title + ", author=" + author + '}';
+        }
+        
+}
--- lab-webapp/src/main/java/com/ethertion/lab/webapp/rest/LibraryRESTController.java
+++ lab-webapp/src/main/java/com/ethertion/lab/webapp/rest/LibraryRESTController.java
@@ -1,11 +1,15 @@
 package com.ethertion.lab.webapp.rest;
 
+import com.ethertion.lab.domain.Author;
 import com.ethertion.lab.domain.Book;
 import com.ethertion.lab.service.BookService;
+import java.util.List;
 import java.util.Optional;
 import org.apache.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
+ * REST Controller for the library.
  * @author amiguel
  */
 @RestController
@@ -26,6 +30,50 @@ public class LibraryRESTController {
         private BookService bookService;
 
         /**
+         * Finds a book by id.
+         *
+         * @param id
+         * @return
+         * @throws Exception
+         */
+        @SuppressWarnings("unchecked")
+        @RequestMapping(method = {RequestMethod.GET}, value = {"/book/{id}"}, produces = "application/json")
+        public @ResponseBody
+        Book findBook(@PathVariable("id") Long id) throws Exception {
+
+                Book book = null;
+                try {
+                        book = (Book) bookService.find(id);
+                } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        throw new Exception("Could not find book by id: " + id, e);
+                }
+
+                return book;
+        }
+
+        /**
+         * Finds all books.
+         * @return
+         * @throws Exception 
+         */
+        @SuppressWarnings("unchecked")
+        @RequestMapping(method = {RequestMethod.GET}, value = {"/books"}, produces = "application/json")
+        public @ResponseBody
+        List<Book> findBooks() throws Exception {
+
+                List<Book> books = null;
+                try {
+                        books = bookService.findAll();
+                } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        throw new Exception("Could not find books", e);
+                }
+
+                return books;
+        }
+
+        /**
          * Finds a book in the library.
          *
          * @param <L>
@@ -35,10 +83,10 @@ public class LibraryRESTController {
          * @throws Exception
          */
         @SuppressWarnings("unchecked")
-        @RequestMapping(method = {RequestMethod.GET}, value = {"/book"}, produces = "application/json")
+        @RequestMapping(method = {RequestMethod.GET}, value = {"/book/title/{title}"}, produces = "application/json")
         public @ResponseBody
-        <L> L findBook(
-                @RequestParam(value = "title", required = true) String title,
+        Book findBookByTitle(
+                @PathVariable("title") String title,
                 HttpServletRequest request) throws Exception {
 
                 Book book = null;
@@ -46,15 +94,60 @@ public class LibraryRESTController {
                         Optional<Book> opt = bookService.findByTitle(title);
                         if (opt.isPresent()) {
                                 book = opt.get();
-                        } else {
-                                book = bookService.save(title);
                         }
                 } catch (Exception e) {
                         logger.error(e.getMessage());
-                        throw new Exception("Could not find book by title: " + title);
+                        throw new Exception("Could not find book by title: " + title, e);
                 }
 
-                return (L) book;
+                return book;
         }
+      
+        /**
+         * Saves a book.
+         * @param in
+         * @return
+         * @throws Exception 
+         */
+        @SuppressWarnings("unchecked")
+        @RequestMapping(method = {RequestMethod.POST}, value = {"/book"}, produces = "application/json")
+        public @ResponseBody
+        Book saveBook(@RequestBody com.ethertion.lab.webapp.pojo.Book in) throws Exception{               
+                
+                Book book = null;
+                
+                try {
+                        Optional<Book> opt = bookService.findByTitle(in.getTitle());
+                        if (opt.isPresent()) {
+                                book = opt.get();
+                        } else {
+                                book = new Book();
+                                book.setTitle(in.getTitle());
+                                Author author = new Author();
+                                author.setFirstName(in.getAuthor());
+                                book.setAuthor(author);
+                                book = bookService.save(book);
+                        }
+                } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        throw new Exception("Could not save book: " + in.toString());
+                }
+
+                return book;
+        }
+        
+        @SuppressWarnings("unchecked")
+        @RequestMapping(method = {RequestMethod.DELETE}, value = {"/book/{id}"}, produces = "application/json")
+        public @ResponseBody
+        void deleteBook(@PathVariable("id") Long id) throws Exception{               
+              
+                try {
+                        bookService.delete(id);
+                } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        throw new Exception("Could not delete book: " + id.toString());
+                }
+                
+        }        
 
 }
